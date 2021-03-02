be7417fc5e4322662508ba09f6584b228400a6c1
Kaitlyn Strickfaden <kaitlyn.strickfaden@gmail.com>
2021-03-02 12:54:34 -0800

    Update app & add data
    Update app to run on sample flume data. Add CSV of flume data joined with image metadata.

--- app.R
+++ app.R
@@ -12,9 +12,8 @@ library(plotly)
 library(lubridate)
 library(htmltools)
 library(htmlwidgets)
-
-
-
+library(DT)
+#source("R/TS_check_image.R")
 
 
 
@@ -39,7 +38,7 @@ ui <- fluidPage(theme = shinytheme("slate"),
                     selectInput(inputId = "Site",
                                 label = "Site",
                                 choices = list.files("www"),
-                                selected = list.files("www")[1]
+                                selected = "TW"
                     ),
                     
                     
@@ -50,8 +49,8 @@ ui <- fluidPage(theme = shinytheme("slate"),
                     
                     selectInput(inputId = "var",
                                 label = "Response",
-                                choices = c("SnowDepth", "Temperature"),
-                                selected = "SnowDepth"
+                                choices = c("Stage"),
+                                selected = "Stage"
                     ),
                     
                     
@@ -75,7 +74,7 @@ ui <- fluidPage(theme = shinytheme("slate"),
                     ## Main Panel Output: ----
                     
                     plotlyOutput("clickplot"), # Time series plot
-                    height = 800,
+                    #height = 800,
                     
                     plotlyOutput("table")
                     
@@ -92,6 +91,7 @@ ui <- fluidPage(theme = shinytheme("slate"),
 
 server <- function(input, output, session) {
   
+  
   output$clickplot <- renderPlotly({
     
     # Find files corresponding to selected site
@@ -103,32 +103,45 @@ server <- function(input, output, session) {
     d <- myImages[str_ends(myImages, ".csv")]
     
     # Find images in wd
-    myImages <- myImages[!str_ends(myImages, ".csv")]
+    #myImages <- myImages[!str_ends(myImages, ".csv")]
     
     # Read in and clean up the data
     dat <- read_csv(d)
     dat <- dat %>%
-      mutate(Time = as.character(Time),
-             Datetime = str_c(Date, Time, sep = " "),
-             Datetime = dmy_hms(Datetime),
-             image_url = myImages) %>%
+      filter(!Stage < 0) %>%
+      mutate(#Time = as.character(Time),
+             #Datetime = str_c(Date, Time, sep = " "),
+             Datetime = ymd_hms(Datetime),
+             image_url = SourceFile) %>%
       arrange(Datetime)
     
-    hdat <- highlight_key(dat, ~image_url)
+    hdat <- highlight_key(dat, ~Datetime)
     # lets you highlight the click event
     
+    
+    ## This commented out bit is supposed to let you choose any column from the data frame as the dependent variable, but I can't get it to work yet... 
+    # outVar <- observe({
+    #   names(dat)
+    # })
+    # 
+    # reactive({
+    #   updateSelectInput(session,
+    #                     inputId = "var",
+    #                     choices = outVar(),
+    #                     selected = input$var)
+    # })
     
     # Allow response variable to be a user input
     
     # if you add more choices here, you will also have to add them
     # to the response variable "selectInput" in the UI.
+    
     data <- reactive({
       switch(input$var,
-             "SnowDepth" = dat$SnowDepth,
-             "Temperature" = dat$Temperature
+             "Stage" = dat$Stage
+             #"Temperature" = dat$Temperature
       )
       })
-    
     
     # Make the plot
     plot_ly(
@@ -145,9 +158,9 @@ server <- function(input, output, session) {
       ),
       hoverinfo = 'y',
       source = "hoverplotsource",
-      customdata = ~ image_url,
+      customdata = ~ image_url#,
       #width  = session$clientData$output_clickplot_width,
-      height = 800
+      #height = 800
     ) %>%
       layout(
         title = str_glue("Time series for {input$Site}"),
@@ -160,9 +173,11 @@ server <- function(input, output, session) {
         ) %>%
       
       event_register('plotly_click') %>% # lets you click points
-      highlight("plotly_click", color = toRGB("red")) %>% # highlights the clicked point
-      event_register('plotly_doubleclick')
+      #event_register('plotly_doubleclick') %>%
+      highlight("plotly_click", color = toRGB("red")) # highlights the clicked point
 
+
+    
   }) # End of renderPlotly
   
   
@@ -204,40 +219,40 @@ server <- function(input, output, session) {
   
   
   
-  doubleclick_event <- reactive({
-    event_data(event = "plotly_doubleclick", source = "hoverplotsource")
-  })
-  
-  
-  # Once a double click happens...
-  observeEvent(doubleclick_event(), {
-    
-    myImages <- str_c("www", input$Site, 
-                      list.files(str_c("www", input$Site, sep = "/") ), 
-                      sep = "/" )
-    
-    # Find csv file in wd
-    d <- myImages[str_ends(myImages, ".csv")]
-    
-    # Find images in wd
-    myImages <- myImages[!str_ends(myImages, ".csv")]
-    
-    # Read in and clean up the data
-    dat <- read_csv(d)
-    dat <- dat %>%
-      mutate(Time = as.character(Time),
-             Datetime = str_c(Date, Time, sep = " "),
-             Datetime = dmy_hms(Datetime),
-             image_url = myImages,
-             OtherWhat = as.character(OtherWhat))
-    
-    output$table <- renderDataTable({
-      #req(event_data("plotly_doubleclick"))
-      dat %>% 
-        filter(customdata == doubleclick_event()$customdata)
-    })
-  
-  })
+  # doubleclick_event <- reactive({
+  #   event_data(event = "plotly_doubleclick", source = "hoverplotsource")
+  # })
+  # 
+  # 
+  # # Once a double click happens...
+  # observeEvent(doubleclick_event(), {
+  #   
+  #   output$table <- DT::renderDataTable({
+  #     
+  #     myImages <- str_c("www", input$Site, 
+  #                       list.files(str_c("www", input$Site, sep = "/") ), 
+  #                       sep = "/" )
+  #     
+  #     # Find csv file in wd
+  #     d <- myImages[str_ends(myImages, ".csv")]
+  #     
+  #     # Find images in wd
+  #     myImages <- myImages[!str_ends(myImages, ".csv")]
+  #     
+  #     # Read in and clean up the data
+  #     dat <- read_csv(d)
+  #     dat <- dat %>%
+  #       mutate(Time = as.character(Time),
+  #              Datetime = str_c(Date, Time, sep = " "),
+  #              Datetime = dmy_hms(Datetime),
+  #              image_url = myImages,
+  #              OtherWhat = as.character(OtherWhat))
+  #     
+  #     dat
+  #     
+  #   })
+  # 
+  # })
   
   
 }
